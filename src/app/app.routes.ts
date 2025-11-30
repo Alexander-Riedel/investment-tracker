e54aef808971fe41992b1511af059fe544f79d6e
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-11-30 11:21:36 +0100

    Add routing configuration for Monthly Overview component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MonthlyOverviewComponent } from './features/monthly-overview/monthly-overview';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'monthly-overview',
+        pathMatch: 'full',
+    },
+    {
+        path: 'monthly-overview',
+        component: MonthlyOverviewComponent,
+    },
+];
